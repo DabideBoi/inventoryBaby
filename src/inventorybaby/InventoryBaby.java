b699a721364374435150373a5a89bde63b003222
DabideBoi <52210045+DabideBoi@users.noreply.github.com>
2020-07-04 17:09:57 +0800

    [NEW UPDATE]
    ADDED:
    - Picture and JText-Description of the selected Item on the list
    - Makes File Receipts and puts them on a list on recept\log.txt
    
    Check for bugs

--- src/inventorybaby/InventoryBaby.java
+++ src/inventorybaby/InventoryBaby.java
@@ -6,6 +6,8 @@
 package inventorybaby;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
@@ -16,6 +18,7 @@ import java.awt.event.ItemListener;
 import java.io.FileNotFoundException;
 import java.awt.event.ActionEvent;
 import java.io.*;
+import java.time.*;
 import java.io.FileReader;
 import java.text.DecimalFormat;
 import java.util.*;
@@ -26,6 +29,10 @@ import java.util.*;
  */
  
 public class InventoryBaby extends JFrame implements ItemListener, ActionListener{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     JFrame Login = new JFrame();
     JLabel labelUsername, labelPassword;
     JTextField textUser;
@@ -39,7 +46,10 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
     DecimalFormat df = new DecimalFormat("0.00");
     
     JFrame Cashier = new JFrame();
-    
+        JFrame Receipt = new JFrame();
+        JTextArea ReceiptDisplay = new JTextArea();
+        JScrollPane ReceiptScroll = new JScrollPane(ReceiptDisplay);
+        JButton ReceiptClose = new JButton("Close");
     String[] cols = {"ID Code", "Amount", "Price", "Name"};
     String[] sel = new String[4];
     DefaultTableModel cart = new DefaultTableModel(cols, 0);
@@ -47,7 +57,7 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
     JComboBox<String> CMBgenre = new JComboBox<String>();
     String genre[] ={"Action.txt", "Adventure.txt", "Comedy.txt", "Romance.txt", "Sci-Fi.txt"};
     ArrayList<String> category = new ArrayList<String>();
-    JButton  logout = new JButton("Logout"), moveToCart = new JButton("Move"), removeToCart = new JButton("Remove");
+    JButton  logout = new JButton("Logout"), moveToCart = new JButton("Move"), removeToCart = new JButton("Remove"), CashOut = new JButton("Cash Out");
     double payment, TPrice, change, priceItem[] = new double[9];
     DecimalFormat decForm = new DecimalFormat("#######.00");
     String idItem[] = new String[9], nameItem[] = new String[9];
@@ -57,8 +67,12 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
     String imageItem[] = new String[9]; 
     ArrayList<String> cartList = new ArrayList<String>();
     JLabel Sum = new JLabel("Php 0");
-    Double cartSum = 0.00;
+    Double cartSum = 0.00, receiptSum = 0.00;
     String movie;
+    int selectedIndex= itemArea.getSelectedIndex();
+    JLabel imgDisplay = new JLabel();
+    JTextArea ItemDescription = new JTextArea();
+
 
     JFrame Admin = new JFrame();
     
@@ -131,17 +145,38 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
         Cashier.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Cashier.setLayout(null);
         Cashier.setTitle("Cashier");
+////////////////////////////////////////////////////////////////////////////////////////////////////        
+        Receipt.setSize(650, 700);
+        Receipt.setUndecorated(true);
+        Receipt.setResizable(false);
+        Receipt.setLayout(null);
+        Receipt.setLocationRelativeTo(null);
+        Receipt.add(ReceiptScroll);
+        Receipt.add(ReceiptClose);
+        ReceiptDisplay.setEditable(false);
+        ReceiptDisplay.setBounds(0, 0, 650, 580);
+        ReceiptScroll.setBounds(0, 0, 650, 580);
+        ReceiptClose.setBounds(280, 600, 120,30);
+        ReceiptClose.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                Receipt.setVisible(false); //you can't see me!
+                Receipt.dispose();
+            }
+        });             
         
+////////////////////////////////////////////////////////////////////////////////////////////////////
         Cashier.add(CMBgenre);
         CMBgenre.setBounds(50,100,200,50);
         CMBgenre.addActionListener(new ActionListener(){ //Genre
             public void actionPerformed(ActionEvent e){
-            	movie = (String) CMBgenre.getSelectedItem();
+                movie = (String) CMBgenre.getSelectedItem();
+                selectedIndex= 0;
 	    		try {
-	    			ProductReader("products//" + movie + ".txt");	
-	    		} catch (Exception x){
+                    ProductReader("products//" + movie + ".txt");	
+	    		} catch (FileNotFoundException x){
 	    			JOptionPane.showMessageDialog(null, "heywaitaminute\n" + x);
-	    		}
+                }
+                Cashier.remove(imgDisplay);
             }
         });
         CMBgenre.addItem("Action");
@@ -149,11 +184,47 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
         CMBgenre.addItem("Comedy");
         CMBgenre.addItem("Romance");
         CMBgenre.addItem("Sci-Fi");
+
+
+        Cashier.add(ItemDescription);
+        ItemDescription.setBounds(200 , 380, 350, 220);
         
+
+
         Cashier.add(LSTmovies); // Cart
         LSTmovies.setBounds(650,160,470,400);
         //LSTmovies.setModel(cart);
         Cashier.add(itemArea); // Movie List
+        itemArea.addListSelectionListener(new ListSelectionListener(){
+            public void valueChanged(ListSelectionEvent e) {
+                /////////////////////////////////////////////////////////////////////////////////////////////////////////
+                try{
+                    int Index = itemArea.getSelectedIndex();
+                    
+                        if(Index >= 0){
+                            ImageIcon Image = new ImageIcon("movie picture//" + movie + " 2//" + idItem[Index] +".jpg");
+                            imgDisplay.setIcon(Image);
+                            int w = Image.getIconWidth();
+                            int h = Image.getIconHeight();
+                            Cashier.add(imgDisplay);
+                            imgDisplay.setBounds(50,380, w, h);// Fix this scale on bulk scaler
+                            ItemDescription.selectAll();
+                            ItemDescription.replaceSelection("");
+                            ItemDescription.append("ID: " + idItem[Index] + "\n");
+                            ItemDescription.append("Name: " + nameItem[selectedIndex]+ "\n");
+                            ItemDescription.append("Price: " + priceItem[Index]+ "\n");
+                            ItemDescription.append("Stocks: " + qtyItem[Index]+ "\n");
+                            
+                    }else{
+                        
+                    }
+                    
+                    }catch(Exception v){
+                        
+                    }
+                
+            }
+        });
         itemArea.setBounds(50,160,500,200);
         
         
@@ -178,14 +249,15 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
             public void tableChanged(TableModelEvent e) {
 
                 cartSum = 0.00;
-                for (int i = 0, rows = cart.getRowCount(); i < rows; i++)
-{                   double quant = Double.parseDouble((String) cart.getValueAt(i, 2));
+                for (int i = 0, rows = cart.getRowCount(); i < rows; i++){                   
+                    double quant = Double.parseDouble((String) cart.getValueAt(i, 2));
                     double presyo = Double.parseDouble((String) cart.getValueAt(i, 3));
                         
                         cartSum += presyo ;
                         System.out.println(quant + " " + presyo + " " + cartSum);
+                        receiptSum = cartSum;
                         Sum.setText("Php" + df.format(cartSum));         
-}
+                }
             }
         });
         Cashier.add(moveToCart);
@@ -193,8 +265,9 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
         moveToCart.addActionListener(new ActionListener(){
            public void actionPerformed(ActionEvent e){
             int count = 0;
+            selectedIndex= 0;
             String selectedItem = (String) itemArea.getSelectedValue();
-            int selectedIndex= itemArea.getSelectedIndex();
+            selectedIndex= itemArea.getSelectedIndex();
                 if (selectedItem == null){
                     JOptionPane.showMessageDialog(null, "Ui walang laman, vro");
                 } 
@@ -210,7 +283,7 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
                                 sel[3] = df.format(waow).toString();
                                 Object[] row = {sel[0],sel[1],sel[2],sel[3]};
                                 cart.addRow(row);
-                                cartSum += newQty*priceItem[selectedIndex];
+                                //make a condition that if the stock is equal to zero then this method will not continue
                                 FileWriter("products//" + movie + ".txt", idItem[selectedIndex], newQty, true);
                                 ProductReader("products//" + movie + ".txt");
                             }
@@ -235,9 +308,23 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
                 String bab = (cart.getValueAt(selectedRowIndex, 0).toString());
                 String babs = (cart.getValueAt(selectedRowIndex, 2).toString());
                 int dabs = Integer.parseInt(babs);
+                String firstThree = bab.substring(0,3);
+                String temp = movie;
+                    switch(firstThree){
+                        case "ACT": movie = "Action";
+                                    break;
+                        case "ADV": movie = "Adventure";
+                                    break;
+                        case "COM": movie = "Comedy";
+                                    break;
+                        case "ROM": movie = "Romance";
+                                    break;
+                        case "SCF": movie = "Sci-Fi";
+                                    break;
+                    }
                 FileWriter("products//" + movie + ".txt", bab, dabs, false);
-                ProductReader("products//" + movie + ".txt");}
-
+                ProductReader("products//" + temp + ".txt");
+            }
                 catch(Exception x){
                     JOptionPane.showMessageDialog(null,x);
     
@@ -260,6 +347,96 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
            }
            
         });
+        Cashier.add(CashOut);
+        CashOut.setBounds(950,580,80,30);
+        CashOut.addActionListener(new ActionListener(){ // This is the Receipt Part
+            public void actionPerformed(ActionEvent e){
+                
+                Double CashIn = Double.parseDouble(JOptionPane.showInputDialog(null, "How much does the customer pay?"));
+                Double change = CashIn - cartSum;
+                int rows = LSTmovies.getRowCount();
+                JOptionPane.showMessageDialog(null, rows);
+                ArrayList<String> receiptList = new ArrayList<String>();
+                String[] lineArr = new String[4]; 
+                Object time = LocalDateTime.now();
+                            String oras = time.toString();
+                            String hulingOras = oras.replace(':','-');
+                if(CashIn < receiptSum)
+                    JOptionPane.showMessageDialog(null, "Insuficient funds");
+                else{
+                    receiptList.add(" ");
+                    receiptList.add(" ");
+                    receiptList.add("-------------------------------------------");
+                    receiptList.add("         BlankBuster Movies Receipt      ");
+                    receiptList.add("-------------------------------------------");
+                    receiptList.add("-------------------------------------------");
+                    receiptList.add(" Movie Name              Amt      T.Price   ");
+                    receiptList.add("-------------------------------------------");
+                    for (int i = 0; i < rows; ++i){  
+                        for(int k = 0; k < 4; ++k)   
+                            lineArr[k] = (String) cart.getValueAt(i, k);
+                        receiptList.add(" "+String.format(" " + "%-24s",lineArr[1])+""+String.format("%-10s",lineArr[2])+""+String.format("%-5s",lineArr[3])+"");
+                    }
+                    receiptList.add("-------------------------------------------");
+                    receiptList.add(" Received    :                     "+(CashIn)+"");
+                    receiptList.add(" Total amount:                     "+(receiptSum)+"");
+                    receiptList.add(" Change      :                     "+(df.format(change))+"");
+                    receiptList.add("-------------------------------------------");
+                    receiptList.add("             Free Home Delivery         ");
+                    receiptList.add("                03111111111             ");
+                    receiptList.add("*******************************************");
+                    receiptList.add("   THANK YOU FOR BUYING FROM BLANKBUSTER");
+                    receiptList.add("*******************************************");
+                        try{
+                            
+                            File resibo = new File("receipts//receipt-" + hulingOras + ".txt");
+                            resibo.canRead();
+                            resibo.canWrite();
+                            PrintWriter RebongJunior = new PrintWriter(resibo);
+                                for(String Manolito : receiptList){ // ahh komedyante ka na nyan?
+                                    RebongJunior.println(Manolito);
+                                }
+                            receiptList.clear();
+                            RebongJunior.close();
+                        }catch(FileNotFoundException ex){
+                            JOptionPane.showMessageDialog(null, "Vro di ko mabasa file mo", "Ui pre wala dito", JOptionPane.OK_OPTION);
+                        }
+                        
+                        try
+                        {
+                        String textLine;
+                        ReceiptDisplay.setTabSize(8);
+                        ReceiptDisplay.setFont(new java.awt.Font("Monospaced", 0, 25));
+                        FileReader fr = new FileReader("receipts//receipt-" + hulingOras + ".txt");
+                        BufferedReader reader = new BufferedReader(fr);
+                        try{
+                            while((textLine=reader.readLine()) != null) {
+                            textLine = reader.readLine();
+                            ReceiptDisplay.read(reader, null);
+                        }
+                        fr.close();
+                        reader.close();
+                    }catch(IOException ek){
+
+                    }
+                    }catch(FileNotFoundException x){
+
+                    }
+                    try
+                        {
+                            Writer output;
+                            output = new BufferedWriter(new FileWriter("receipts//" + "log.txt", true));  //clears file every time
+                            output.append("receipt-" + hulingOras + "\n");
+                            output.close();
+                    }catch(IOException ek){
+
+                    }
+                    Receipt.setVisible(true);
+                }
+                
+            }
+        }
+    );
         Cashier.add(Sum);
         Sum.setBounds(1050,580,80,30);
         
@@ -324,6 +501,7 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
             ArrayList<String> yoinkList = new ArrayList<String>();
             int Sabuco; 
             Scanner input = new Scanner(new FileReader(txtFile));
+            boolean confirm;
             while(input.hasNextLine()){
                 String line = input.nextLine(); String[] lineArr = line.split("/");
                     int k = 0;
@@ -332,7 +510,12 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
                             Sabuco = Integer.parseInt(lineArr[2]) - itemStock;
                         else
                             Sabuco = Integer.parseInt(lineArr[2]) + itemStock;
-                        yoinkList.add(lineArr[0] + "/" + lineArr[1] + "/" + Sabuco + "/" + lineArr[3] /* + "/" + picID     remove comments if picID is ready*/);
+                        if (Sabuco < 0){
+                            JOptionPane.showMessageDialog(null, "Wala nang stocks dude");
+                            yoinkList.add(line);
+                        }
+                        else
+                            yoinkList.add(lineArr[0] + "/" + lineArr[1] + "/" + Sabuco + "/" + lineArr[3] /* + "/" + picID     remove comments if picID is ready*/);
                     }
                     else{
                         yoinkList.add(line);
@@ -373,9 +556,9 @@ public class InventoryBaby extends JFrame implements ItemListener, ActionListene
             }
             input.close();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
    /* public void ProductWriter(String textFile, int quantTxt, int quantInput){
             try{
